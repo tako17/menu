bd379bf33fb52ef943ab30ef8aaefb7b1e3cd3b4
tako17 <tako.chokoraia2000@gmail.com>
2022-05-18 23:20:39 +0400

    full

---
+++ js/togle.js
@@ -0,0 +1,16 @@
+"use strict"
+const burgerNav=document.querySelector('.mobile-nav')
+const burgerMenu=document.querySelector('.humburger-menu')
+
+
+
+
+burgerMenu.addEventListener(
+
+'click',()=>{
+    
+    burgerNav.classList.toggle('hidden')
+   
+}
+
+)
